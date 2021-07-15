6d44f62c5eaea2d270b25d0d178673d5191b390b
sebastian zapata <sebastianzapata@MacBook-Pro-de-sebastian.local>
2021-07-15 10:31:05 -0500

    adding initial spec to registrations controller and routing

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class RegistrationsController < DeviseTokenAuth::RegistrationsController
-      skip_before_action :verify_authenticity_token
+      # skip_before_action :verify_authenticity_token
 
       private
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1, defaults: { format: :json } do
-      # devise_scope :user do
-        # resource :user, only: %i[update show]
-      # end
     end
   end
 end
