f3142f078f13ed1cf670de0c8a1e8c38d36c5014
sebastian zapata <sebastianzapata@MacBook-Pro-de-sebastian.local>
2021-07-16 11:15:58 -0500

    removing commentaries

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,8 +1,6 @@
 module Api
   module V1
     class RegistrationsController < DeviseTokenAuth::RegistrationsController
-      # skip_before_action :verify_authenticity_token
-
       private
 
       def sign_up_params
